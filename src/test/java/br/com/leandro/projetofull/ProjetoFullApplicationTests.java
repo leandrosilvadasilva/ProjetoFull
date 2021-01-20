bd3888fdaaf6081b58d9cf41729c54deedc88e81
Leandro Silva da Silva <leandro.tecnicoinfo@gmail.com>
2021-01-20 10:42:17 -0300

    criacao e teste projeto full

---
+++ src/test/java/br/com/leandro/projetofull/ProjetoFullApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.leandro.projetofull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoFullApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
