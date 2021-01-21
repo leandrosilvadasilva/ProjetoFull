7b4abf4d1013f696de13f528efc05b9aa6ffc6fe
Leandro Silva da Silva <leandro.tecnicoinfo@gmail.com>
2021-01-21 10:11:59 -0300

    banco de dados h2 e criacao automatica de base de dados

--- src/main/java/br/com/leandro/projetofull/domain/Categoria.java
+++ src/main/java/br/com/leandro/projetofull/domain/Categoria.java
@@ -2,11 +2,16 @@ package br.com.leandro.projetofull.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria implements Serializable{
 
 
 	private static final long serialVersionUID = 1L;
-
+	
+	@Id
 	private Integer id;
 	private String name;
 	public Categoria() {
