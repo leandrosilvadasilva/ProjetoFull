e411468a5669b55d84296a52f80da7bd7966c054
Leandro Silva da Silva <leandro.tecnicoinfo@gmail.com>
2021-01-24 19:59:57 -0300

    criando operacao de instaciacao

--- src/main/java/br/com/leandro/projetofull/ProjetoFullApplication.java
+++ src/main/java/br/com/leandro/projetofull/ProjetoFullApplication.java
@@ -1,13 +1,38 @@
 package br.com.leandro.projetofull;
 
+import java.util.Arrays;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ProjetoFullApplication {
+import br.com.leandro.projetofull.domain.Categoria;
+import br.com.leandro.projetofull.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class ProjetoFullApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoFullApplication.class, args);
 	}
+
+	//Instanciar objetos
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+//		Categoria cat3 = new Categoria(null, "");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
+	}
+	
+	
 
 }
